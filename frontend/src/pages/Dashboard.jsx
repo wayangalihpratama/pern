48f707863ff76f546337b7d2a031f7f0f9d067ae
wayangalihpratama <wgprtm@gmail.com>
2022-10-26 18:48:49 +0800

    Fetch access token using refresh token

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Container } from "../components";
-import { store } from "../lib";
+import { api, store } from "../lib";
+import jwt_decode from "jwt-decode";
 
 const Dashboard = () => {
   const loading = store.ui.useState((s) => s.loading);
@@ -8,11 +9,24 @@ const Dashboard = () => {
 
   useEffect(() => {
     if (loading) {
-      setTimeout(() => {
-        store.ui.update((s) => {
-          s.loading = false;
+      api
+        .get("/refresh_token")
+        .then((res) => {
+          const { accessToken } = res.data;
+          api.setToken(accessToken);
+          const user = jwt_decode(accessToken);
+          store.data.update((s) => {
+            s.user = user;
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          store.ui.update((s) => {
+            s.loading = false;
+          });
         });
-      }, 500);
     }
   }, [loading]);
 
